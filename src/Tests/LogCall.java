70296c0e52e5897498f9d3b6d272c9b28ff2160c
esmaAkcagun <esmaakcagunn@gmail.com>
2020-05-12 16:46:12 +0200

    add Practice.java

--- src/Tests/LogCall.java
+++ src/Tests/LogCall.java
@@ -1,4 +1,7 @@
 package Tests;
 
 public class LogCall {
+    public static void main(String[] args) {
+        System.out.println("calling master..");
+    }
 }
---
+++ src/Tests/Practice.java
@@ -0,0 +1,8 @@
+package Tests;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("let's practice");
+    }
+}
