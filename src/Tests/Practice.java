769b40fdef5de74f25140da869f195d563dbd5c5
esmaAkcagun <62145736+esmaAkcagun@users.noreply.github.com>
2020-05-12 17:05:09 +0200

    Update Practice.java

--- src/Tests/Practice.java
+++ src/Tests/Practice.java
@@ -4,5 +4,7 @@ public class Practice {
 
     public static void main(String[] args) {
         System.out.println("let's practice");
+        
+        System.out.println("let's learn how to pull");
     }
 }
